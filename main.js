1c059f061ae28ebd909bc0b9b9568de14dfb129d
ihjhyr <94471194+ihjhyr@users.noreply.github.com>
2022-03-12 11:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.position(10, 50);
+
+    canvas = createCanvas(800, 400);
+    canvas.position(430, 130);
+
+    poseNet = ml5.poseNet(video, modelDone);
+    poseNet.on('pose', gotposes);
+}
+
+function draw() {
+    background("#5196e3");
+}
+
+function modelDone() {
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results, error) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
